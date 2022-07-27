0a64a20f545d95adbd706776f160dc28ba8109bf
Kenichi Kamiya <kachick1@gmail.com>
2022-07-28 05:10:40 +0900

    Remove needless union

--- scripts/validator.cr
+++ scripts/validator.cr
@@ -1,6 +1,6 @@
 module RenovateConfigAsdf
   module Validator
-    def self.validate(globs : Array(String)) : Tuple(Bool, String | Nil)
+    def self.validate(globs : Array(String)) : Tuple(Bool, String)
       Dir.glob(globs).each do |path|
         status = Process.run(env: ENV.to_h.merge({"RENOVATE_CONFIG_FILE" => path}), command: "npx", args: ["renovate-config-validator"], output: STDOUT)
         return({false, "#{path} has incorrect definitions"}) unless status.success?
