c5fa06f733bcd436aaf828e49a651da454d70ea7
Kenichi Kamiya <kachick1@gmail.com>
2022-07-28 05:14:54 +0900

    Remove historycal shell script

--- scripts/rename_to_subdir.2022-07-17.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -eux
-
-for old_path in $(git ls-files 'asdf-plugin-*.json'); do
-  mv "$old_path" "plugins/$(echo "$old_path" | grep -Po '(?<=asdf-plugin-)\S+?(?=\.json)').json5"
-done
