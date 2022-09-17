49f553e562c502971b0497983e6b20631b5a4811
Kenichi Kamiya <kachick1@gmail.com>
2022-09-17 21:08:26 +0900

    Remove duplicated crystal code

--- src/renovate_cofig_asdf.cr
+++ src/renovate_cofig_asdf.cr
@@ -1,11 +1,4 @@
 require "./renovate_cofig_asdf/*"
 
 module RenovateConfigAsdf
-  def self.defined_plugin_files : Array(String)
-    Dir.entries("plugins").reject { |basename| basename == "." || basename == ".." }
-  end
-
-  def self.defined_plugins : Array(String)
-    defined_plugin_files.map(&.[%r<([\S]+)\.json5\z>, 1])
-  end
 end
