6a160a43696b951f3a74b348b604b5800e59f3de
Kenichi Kamiya <kachick1@gmail.com>
2022-10-03 23:06:46 +0900

    Support .tool-versions updating in scaffolder

--- spec/scaffolder_spec.cr
+++ spec/scaffolder_spec.cr
@@ -1,5 +1,7 @@
 require "./spec_helper"
 
+require "json"
+
 describe RenovateConfigAsdf::Scaffolder do
   describe ".scaffold" do
     it "generates JSON from given plugin name" do
@@ -28,6 +30,58 @@ describe RenovateConfigAsdf::Scaffolder do
     it "works even if given entry will be last position" do
       entires = %w(bun crystal)
       RenovateConfigAsdf::Scaffolder.insert_entry(entires, "ruby").should eq(%w(bun crystal ruby))
+    end
+  end
+
+  describe ".updated_defaullt_json" do
+    json_format = Hash(String, String | Array(String))
+
+    it "returns inserted JSON into correct position it is might be unformatted" do
+      origin = <<-JSON
+      {
+        "$schema": "https://docs.renovatebot.com/renovate-schema.json",
+        "description": "Sharable config for .tool-version with asdf",
+        "extends": [
+          "local>kachick/renovate-config-asdf//plugins/crystal.json5",
+          "local>kachick/renovate-config-asdf//plugins/deno.json5",
+          "local>kachick/renovate-config-asdf//plugins/rust.json5"
+        ]
+      }
+      JSON
+
+      expected = <<-JSON
+      {
+        "$schema": "https://docs.renovatebot.com/renovate-schema.json",
+        "description": "Sharable config for .tool-version with asdf",
+        "extends": [
+          "local>kachick/renovate-config-asdf//plugins/crystal.json5",
+          "local>kachick/renovate-config-asdf//plugins/deno.json5",
+          "local>kachick/renovate-config-asdf//plugins/ruby.json5",
+          "local>kachick/renovate-config-asdf//plugins/rust.json5"
+        ]
+      }
+      JSON
+
+      json_format.from_json(RenovateConfigAsdf::Scaffolder.updated_defaullt_json("ruby", origin)).should eq(json_format.from_json(expected))
+    end
+  end
+
+  describe ".updated_example" do
+    it "returns inserted string into correct position" do
+      origin = <<-ASDF
+      crystal 1.5.1
+      deno 1.25.3
+      rust 1.63.0
+      ASDF
+
+      expected = <<-ASDF
+      crystal 1.5.1
+      deno 1.25.3
+      ruby <UPDATEME!>
+      rust 1.63.0
+      ASDF
+
+      RenovateConfigAsdf::Scaffolder.updated_example("ruby", origin).should eq(expected)
     end
   end
 end
--- src/renovate_cofig_asdf/scaffolder.cr
+++ src/renovate_cofig_asdf/scaffolder.cr
@@ -5,6 +5,9 @@ require "json"
 
 module RenovateConfigAsdf
   class Scaffolder
+    DEAFULT_JSON_PATH = "default.json"
+    EXAMPLE_TOOL_VERSIONS_PATH = "examples/.tool-versions"
+
     def initialize(@plugin : String)
     end
 
@@ -20,15 +23,28 @@ module RenovateConfigAsdf
       [*entries[0, position], inserting, *entries[position, entries.size]]
     end
 
-    def self.write(plugin : String)
-      json = Hash(String, String | Array(String)).from_json(File.read("default.json"))
+    def self.updated_defaullt_json(plugin : String, origin : String) : String
+      json = Hash(String, String | Array(String)).from_json(origin)
       entries = json["extends"]
       raise "Unexpected schema of JSON" unless entries.is_a?(Array(String))
       new_entry = "local>kachick/renovate-config-asdf//plugins/#{plugin}.json5"
       json["extends"] = insert_entry(entries, new_entry)
+      json.to_json
+    end
 
-      File.write("plugins/#{plugin}.json5", scaffold(plugin))
-      File.write("default.json", json.to_json)
+    def self.updated_example(plugin : String, origin : String) : String
+      entires = origin.lines(chomp: false)
+      insert_entry(entires, "#{plugin} <UPDATEME!>\n").join
+    end
+
+    def self.write(plugin : String)
+      config = scaffold(plugin)
+      new_json = updated_defaullt_json(plugin, File.read(DEAFULT_JSON_PATH))
+      new_example = updated_example(plugin, File.read(EXAMPLE_TOOL_VERSIONS_PATH))
+
+      File.write("plugins/#{plugin}.json5", config)
+      File.write(DEAFULT_JSON_PATH, new_json)
+      File.write(EXAMPLE_TOOL_VERSIONS_PATH, new_example)
     end
   end
 end
