a103a83aab01969fee8abb292bef46b3d1724325
Kenichi Kamiya <kachick1@gmail.com>
2022-10-27 17:54:24 +0900

    Drop npm dependency when running crystal specs in CI (#274)

---
+++ spec/fixtures/mock-validator.bash
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+env | grep RENOVATE_CONFIG_FILE
--- spec/validator_spec.cr
+++ spec/validator_spec.cr
@@ -1,19 +1,28 @@
 require "./spec_helper"
 
 describe RenovateConfigAsdf::Validator do
-  it "returns true for empty globs" do
-    success, _message = RenovateConfigAsdf::Validator.validate(%w[])
-    success.should eq(true)
+  context "with mock execution" do
+    it "passes RENOVATE_CONFIG_FILE as an ENV" do
+      success, _message = RenovateConfigAsdf::Validator.validate(%w[default.json], command: "spec/fixtures/mock-validator.bash")
+      success.should eq(true)
+    end
   end
 
-  it "returns true for actual globs" do
-    success, _message = RenovateConfigAsdf::Validator.validate(%w[default.json])
-    success.should eq(true)
-  end
+  context "with actual npm command", tags: "needs_npm" do
+    it "returns true for empty globs" do
+      success, _message = RenovateConfigAsdf::Validator.validate(%w[])
+      success.should eq(true)
+    end
 
-  it "returns false for invalid definiton" do
-    success, message = RenovateConfigAsdf::Validator.validate(%w[spec/fixtures/ruby-invalid.json5])
-    success.should eq(false)
-    message.should contain("spec/fixtures/ruby-invalid.json5 has incorrect definitions")
+    it "returns true for actual globs" do
+      success, _message = RenovateConfigAsdf::Validator.validate(%w[default.json])
+      success.should eq(true)
+    end
+
+    it "returns false for invalid definiton" do
+      success, message = RenovateConfigAsdf::Validator.validate(%w[spec/fixtures/ruby-invalid.json5])
+      success.should eq(false)
+      message.should contain("spec/fixtures/ruby-invalid.json5 has incorrect definitions")
+    end
   end
 end
