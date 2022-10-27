e57f615f228537d382dd091ecc1bba622258261e
Kenichi Kamiya <kachick1@gmail.com>
2022-10-27 16:37:31 +0900

    Revert "Ah, it should be json"
    This reverts commit 45ecac0994b435b3b97356548c69a9c9e27c9727.

--- src/renovate_config_asdf/github_actions_helper.cr
+++ src/renovate_config_asdf/github_actions_helper.cr
@@ -5,7 +5,7 @@ module RenovateConfigAsdf
     def self.generate_matrix(text : String, chunk_size : UInt16) : String
       lines = text.lines.map(&.chomp)
       raise ArgumentError.new("given text having unexpected whitespaces") if lines.any? { |line| /\s/.matches?(line) }
-      lines.each_slice(chunk_size).map(&.join(" ").to_json).to_json
+      lines.each_slice(chunk_size).map(&.join(" ")).to_json
     end
   end
 end
