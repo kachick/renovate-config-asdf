d7d8acdfb7d7a1f69abb1b7285d5c48629d8c5f4
Kenichi Kamiya <kachick1@gmail.com>
2023-04-25 19:06:12 +0900

    Bump nixpkgs to latest to update crystal version

--- shell.nix
+++ shell.nix
@@ -1,9 +1,9 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/4c3edba85629ec304b5269e4b0ac7f26c433df23.tar.gz") { } }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/f3f15fa73ddf15fa66cc4f80cda4c5afb356306b.tar.gz") { } }:
 
 pkgs.mkShell {
   buildInputs = [
     pkgs.nodejs-18_x
-    pkgs.crystal_1_7
+    pkgs.crystal_1_8
     pkgs.shards
     pkgs.dprint
     pkgs.nil
