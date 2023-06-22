8abd3de61377ea4fd899e57d0c6c3f28d05ed80b
Kenichi Kamiya <kachick1@gmail.com>
2023-06-22 20:26:41 +0900

    Wait a minutes to execute many process in validator
    Because of it often fail in https://github.com/kachick/renovate-config-asdf/actions/runs/5344828927/jobs/9689692931

--- src/renovate_config_asdf/validator.cr
+++ src/renovate_config_asdf/validator.cr
@@ -4,6 +4,7 @@ module RenovateConfigAsdf
   module Validator
     def self.validate(globs : Array(String), command : String = "npx", args : Array(String) = ["renovate-config-validator"]) : Tuple(Bool, String)
       processes = Dir.glob(globs).to_h do |path|
+        sleep(0.5)
         {path, Process.new(env: ENV.to_h.merge({"RENOVATE_CONFIG_FILE" => path}), command: command, args: args, output: STDOUT, error: STDERR)}
       end
       failures = processes.reject do |_path, process|
