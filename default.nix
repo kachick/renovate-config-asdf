daf2623308cb363f03277755e508e8d25ddc78a4
Kenichi Kamiya <kachick1@gmail.com>
2023-07-23 11:39:07 +0900

    Update nixpath to use crystalline
    https://github.com/NixOS/nixpkgs/commit/19a336ffabcd7d77cb3d55fa890ab64e7c4ac3f3

--- default.nix
+++ default.nix
@@ -1,14 +1,18 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/21061e4e4a0f46a6f487c7583be916e4e031ee4e.tar.gz") { } }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/19a336ffabcd7d77cb3d55fa890ab64e7c4ac3f3.tar.gz") { } }:
 
 pkgs.mkShell {
   buildInputs = [
     pkgs.nodejs-18_x
-    pkgs.crystal
-    pkgs.shards
     pkgs.dprint
     pkgs.nil
     pkgs.nixpkgs-fmt
-    pkgs.pcre
     pkgs.typos
+
+    # Section for crystal
+    pkgs.crystal
+    pkgs.shards
+    # Used in crystal
+    pkgs.pcre
+    pkgs.crystalline
   ];
 }
