0a0f4526837a2adaaa52b7bcb78dbeb2d6363ee3
Kenichi Kamiya <kachick1@gmail.com>
2023-07-26 23:40:30 +0900

    Replace eslint with deno_lint

--- test/regex_test.ts
+++ test/regex_test.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import test from 'node:test';
 import assert from 'node:assert';
 import fs from 'fs';
